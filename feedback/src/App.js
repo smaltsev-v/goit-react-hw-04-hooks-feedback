caef61570b02f3bb04ec339ec237699b1113200e
Serhii Maltsev <83024347+smaltsev-v@users.noreply.github.com>
2022-01-18 22:05:11 +0200

    fix

--- feedback/src/App.js
+++ feedback/src/App.js
@@ -4,15 +4,10 @@ import Statistics from './components/Statistics';
 import Section from "./components/Section";
 import './App.css'
 
-
-
-
 export default function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  
   
   const btnClickFeedback = option  => {
      
@@ -31,40 +26,26 @@ export default function App() {
     }
   };
   
-  
-   const countTotalFeedback = () => {
-    return good + neutral + bad;
-  };
+  const countTotalFeedback = good + neutral + bad;
+  const countPositiveFeedbackPercentage = Math.round((good / countTotalFeedback) * 100) || 0;
 
-
-   const countPositiveFeedbackPercentage = () => {
-    return Math.round((good / countTotalFeedback()) * 100) || 0;
-  };
-
-  
-
-
-
-    
-    
-
-    return (
-      <div className="App">
-        <Section title="Please leave feedback:">
-          <Control
-           options={['good', 'neutral', 'bad']}
-            onLeaveFeedback={btnClickFeedback}
-          />
-          <h1>Statistics:</h1>
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={countTotalFeedback}
-            positivePercentage={countPositiveFeedbackPercentage()}
-          />
-        </Section>
-      </div>
-    );
+  return (
+    <div className="App">
+      <Section title="Please leave feedback:">
+        <Control
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={btnClickFeedback}
+        />
+        <h1>Statistics:</h1>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback}
+          positivePercentage={countPositiveFeedbackPercentage}
+        />
+      </Section>
+    </div>
+  );
 };
- 
+
